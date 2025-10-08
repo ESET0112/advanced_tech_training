a08bb5b33a8bf6e0d12df6f3d73250c1f7de364f
Nilanjan <nilanjan.chakraborty@esyasoft.com>
2025-10-08 12:15:07 +0530

    Added CS.1.012 of OOPs Assignment

---
+++ .net/assignments/02.oops/CS.1.012/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Reading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tariff Domestic = new Tariff("Domestic");
+            Tariff Commercial = new Tariff("Commercial",8.0);
+            Tariff Agri = new Tariff("Agri",9.0,80);
+
+            Domestic.ComputeBill(120);
+            Commercial.ComputeBill(120);
+            Agri.ComputeBill(120);
+        }
+    }
+    internal class Tariff
+    {
+        public string Name { get; set; }
+        public double RatePerKwh { get; set; }
+        public double FixedCharge {  get; set; }
+
+        public Tariff(string name) 
+        {
+            Name = name;
+            RatePerKwh = 6.0;
+            FixedCharge = 50;
+        } 
+
+        public Tariff(string name, double rate)
+        {
+            Name = name;
+            RatePerKwh = rate;
+            FixedCharge = 50;
+        }
+
+        public Tariff(string name, double rate, double fixedCharge)
+        {
+            Name = name;
+            RatePerKwh = rate;
+            FixedCharge = fixedCharge;
+        }
+        public void ComputeBill(int units)
+        {
+            Console.WriteLine($"{Name}: {units * RatePerKwh + FixedCharge}"); 
+        }
+    }
+}
