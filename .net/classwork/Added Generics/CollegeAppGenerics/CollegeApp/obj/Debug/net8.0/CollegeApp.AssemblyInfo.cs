dbebe374dea67fba1458bcbd8f5070bc664cbf70
Nilanjan <nilanjan.chakraborty@esyasoft.com>
2025-10-31 10:52:55 +0530

    Project with Role Based Authentication with Web API and Razor Pages

--- .net/classwork/Added Generics/CollegeAppGenerics/CollegeApp/obj/Debug/net8.0/CollegeApp.AssemblyInfo.cs
+++ .net/classwork/Added Generics/CollegeAppGenerics/CollegeApp/obj/Debug/net8.0/CollegeApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CollegeApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e329e12ae2fc9beddc511b3c00efa1fb1c01d23f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb6b0d5628e950afec1c089f3986231e3279804")]
 [assembly: System.Reflection.AssemblyProductAttribute("CollegeApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CollegeApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
